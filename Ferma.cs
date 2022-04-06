cf88391be2060a2d0177f4134a3ac6c03f69f966
Алмаз Суфиянов <fkvfpbr1804@gmail.com>
2022-04-06 22:18:05 +0300

    First commit

---
+++ Ferma.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Security.Cryptography;
+
+
+namespace Ferma
+{
+    public static class Fermas
+    {
+        public static BigInteger result;
+        public static bool flag=true;
+        public static bool FermaAlg(BigInteger a, int times)
+        {
+            if (a == 2||a==3 || a == 5)
+            {
+                
+                return true;
+            }
+            if (a % 2==0|| a % 3 == 0|| a % 5 == 0)
+            {
+                if (a == 561 || a == 1105 || a == 1729)
+                {
+                    return true;
+                }
+                return false;
+            }
+            
+            /*1.Берется рандомное число<a
+            2.Проверяется(a, p) == 1
+            3.Проверяется нужное уравнение
+            4.Повторяется times раз*/
+
+
+            BigInteger p=a;
+
+            for (int i = 0; i < times; i++)
+            {
+                do
+                { 
+                        p = getRandom(times, 2,a-1);
+                    
+                }
+                while (BigInteger.GreatestCommonDivisor(a, p) != 1);
+
+                if (BigInteger.ModPow(p,a - 1, a) != 1)
+                {
+                    return false;
+                }
+               
+            }
+            return true;
+           
+
+
+        }
+
+
+
+        //minimum
+        public static BigInteger minimum(int length)
+        {
+            BitArray b = new BitArray(length);
+            b.SetAll(false);
+            b.Set(length - 1, true);
+
+            byte[] m = b.ToBytes().ToArray();
+            BigInteger res = new BigInteger(m.Concat(new byte[] { 0 }).ToArray());
+            return res;
+        }
+
+
+        //maximum
+        public static BigInteger maximum(int length)
+        {
+            BitArray b = new BitArray(length);
+            b.SetAll(true);
+            byte[] m = b.ToBytes().ToArray();
+            BigInteger res = new BigInteger(m.Concat(new byte[] { 0 }).ToArray());
+            return res;
+        }
+
+
+        //Рандом от 0 до числа
+        public static BigInteger getRandom(int length, BigInteger min, BigInteger max)
+        {
+            BigInteger num;
+            Random random = new Random();
+            BitArray bitarr = new BitArray(length);
+            byte[] bytearr = ToBytes(bitarr).ToArray();
+         
+            
+
+            do
+            {
+                random.NextBytes(bytearr);
+                
+                num = new BigInteger(bytearr.Concat(new byte[] { 0 }).ToArray());
+                
+            } while (num > max||num < min);
+            return num;
+
+        }
+
+        public static IEnumerable<byte> ToBytes(this BitArray bits, bool MSB = false)
+        {
+            int bitCount = 7;
+            int outByte = 0;
+
+            foreach (bool bitValue in bits)
+            {
+                if (bitValue)
+                    outByte |= MSB ? 1 << bitCount : 1 << (7 - bitCount);
+                if (bitCount == 0)
+                {
+                    yield return (byte)outByte;
+                    bitCount = 8;
+                    outByte = 0;
+                }
+                bitCount--;
+            }
+            // Последний частично декодированный байт
+            if (bitCount < 7)
+                yield return (byte)outByte;
+        }
+
+        public static BigInteger MyModPow(BigInteger Number, BigInteger Pow, BigInteger Mod)
+        {
+            BigInteger Result = 1;
+            BigInteger Bit = Number % Mod;
+
+            while (Pow > 0)
+            {
+                if ((Pow & 1) == 1)
+                {
+                    Result *= Bit;
+                    Result %= Mod;
+                }
+                Bit *= Bit;
+                Bit %= Mod;
+                //битовый сдвиг, вправо - целочисленное деление на 2, влево - умножение на 2
+                Pow >>= 1;
+            }
+            return Result;
+        }
+
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Numerics;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace Ferma
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int Log2n(BigInteger n)
+        {
+            if (n < 1)
+            {
+                return -1;
+            }
+            BigInteger x = 0;
+            BigInteger num = 1;
+            while (num <= n)
+            {
+                x++;
+                num *= 2;
+            }
+            return (int)x;
+        }
+        //генератор
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            //0 -Ферма
+            Stopwatch clock = new Stopwatch();
+            if (Mode.SelectedIndex == 0)
+            {
+                clock.Start();
+                if (BitSize.Text == "")
+                {
+                    MessageBox.Show("Введите число бит");
+                    return;
+                }
+                int bits = int.Parse(BitSize.Text);
+                BigInteger num = 0;
+
+                do
+                {
+
+                    num = Fermas.getRandom(bits, Fermas.minimum(bits), Fermas.maximum(bits));
+
+
+                } while (!Fermas.FermaAlg(num, bits));
+                clock.Stop();
+                ResultNumber.Text = num.ToString();
+               
+                Error.Text = Math.Pow(2, -bits).ToString();
+                Time1.Text = clock.ElapsedMilliseconds.ToString() + " миллисекунд";
+            }
+            //1- Миллер-Рабин
+/*            else 
+            {
+                if (BitSize.Text == "")
+                {
+                    MessageBox.Show("Введите число бит");
+                    return;
+                }
+                clock.Start();
+                int bits = int.Parse(BitSize.Text);
+                BigInteger num = 0;
+                do
+                {
+
+                    num = Fermas.getRandom(bits, Fermas.minimum(bits), Fermas.maximum(bits));
+
+
+                } while (!M_R.MRTest(num,Log2n(num)));
+                clock.Stop();
+                ResultNumber.Text = num.ToString();
+                Error.Text = Math.Pow(4, -Log2n(num)).ToString();
+                MessageBox.Show("Затраченное время " + clock.ElapsedMilliseconds / 1000 + " секунд(" + clock.ElapsedMilliseconds + " миллисекунд)");
+            }*/
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch clock = new Stopwatch();
+            //0-Ferma
+            if (Mode.SelectedIndex==0)
+            {
+                if (NumberForChecking.Text == "")
+                {
+                    MessageBox.Show("Введите число");
+                    return;
+                }
+                clock.Start();
+                BigInteger a = BigInteger.Parse(NumberForChecking.Text);
+
+                int bits = a.ToByteArray().Length * 8;//int.Parse(BitSize.Text);
+                if (Fermas.FermaAlg(a, bits))
+                {
+                    clock.Stop();
+                    MessageBox.Show("Число вероятно простое");
+                    Error.Text = Math.Pow(2, -bits).ToString();
+                }
+                else
+                {
+                    clock.Stop();
+                    MessageBox.Show("Число составное");
+                    Error.Text = "0";
+                }
+                Time1.Text = clock.ElapsedMilliseconds.ToString() + " миллисекунд";
+                
+
+            }
+            //1-M-R
+          /*  else 
+            {
+                if (NumberForChecking.Text == "")
+                {
+                    MessageBox.Show("Введите число");
+                    return;
+                }
+                
+                clock.Start();
+                int log = Log2n(BigInteger.Parse(NumberForChecking.Text));
+                if (M_R.MRTest(BigInteger.Parse(NumberForChecking.Text),log))
+                {
+                    clock.Stop();
+                    MessageBox.Show("Число вероятно простое");
+                    Error.Text = Math.Pow(4, -log).ToString();
+                }
+                else
+                {
+                    clock.Stop();
+                    MessageBox.Show("Число составное");
+                    Error.Text = "0";
+                }
+                MessageBox.Show("Затраченное время " + clock.ElapsedMilliseconds / 1000 + " секунд(" + clock.ElapsedMilliseconds + " миллисекунд)");
+            }*/
+
+
+        }
+
+        //ввод в бит
+        private void BitSize_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            /*if (Regex.IsMatch(e.Text, "@[^0-9]"))
+            {
+                e.Handled = true;
+            }*/
+            e.Handled = "0123456789".IndexOf(e.Text) < 0;
+        }
+        //вставка в бит
+        private void BitSize_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string TextFotChecking = (string)e.DataObject.GetData(typeof(String));
+            if (Regex.IsMatch(TextFotChecking, @"[^0-9]"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        //ввод в число
+        private void NumberForChecking_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = "0123456789".IndexOf(e.Text) < 0;
+        }
+
+        //вставка в число
+        private void NumberForChecking_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string TextFotChecking = (string)e.DataObject.GetData(typeof(String));
+            if (Regex.IsMatch(TextFotChecking, @"[^0-9]"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void NumberForChecking_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Space)
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        private void BitSize_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Space)
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        private void Mode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+
+        private void SecCheckBut_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch clock = new Stopwatch();
+            if (NumberForChecking.Text == "")
+            {
+                MessageBox.Show("Введите число");
+                return;
+            }
+
+            clock.Start();
+            int log = Log2n(BigInteger.Parse(NumberForChecking.Text));
+            if (M_R.MRTest(BigInteger.Parse(NumberForChecking.Text), log))
+            {
+                clock.Stop();
+                MessageBox.Show("Число вероятно простое");
+                SecError.Text = Math.Pow(4, -log).ToString();
+            }
+            else
+            {
+                clock.Stop();
+                MessageBox.Show("Число составное");
+                SecError.Text = "0";
+            }
+            Time2.Text = clock.ElapsedMilliseconds.ToString() + " миллисекунд";
+            
+        }
+
+        private void SecGenBut_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch clock = new Stopwatch();
+            if (BitSize.Text == "")
+            {
+                MessageBox.Show("Введите число бит");
+                return;
+            }
+            clock.Start();
+            int bits = int.Parse(BitSize.Text);
+            BigInteger num = 0;
+            do
+            {
+
+                num = Fermas.getRandom(bits, Fermas.minimum(bits), Fermas.maximum(bits));
+
+
+            } while (!M_R.MRTest(num, Log2n(num)));
+            clock.Stop();
+            SecNumber.Text = num.ToString();
+            SecError.Text = Math.Pow(4, -Log2n(num)).ToString();
+            Time2.Text = clock.ElapsedMilliseconds.ToString() + " миллисекунд";
+           
+        }
+    }
+}
+
